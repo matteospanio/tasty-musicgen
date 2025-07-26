6d08f88a6a1b483376abbe1988bc0bfa8df7c2b6
Matteo <spartacus990@gmail.com>
2025-07-26 20:40:41 +0200

    Add batch cli
    In order to be able to process many files at the same time a batch cli
    command has been added

--- src/tasty_musicgen/cli.py
+++ src/tasty_musicgen/cli.py
@@ -1,6 +1,9 @@
 from rich.prompt import Prompt
 from rich.console import Console
 import uuid
+import click
+from pathlib import Path
+from rich.progress import track
 
 from tasty_musicgen.model import get_model, make_audio_from_text
 from tasty_musicgen.utils import save_audio
@@ -28,3 +31,54 @@ def main() -> None:
 
     # Save the audio to a file
     save_audio(music.squeeze(0), f"{uuid.uuid1()}", synthesiser)
+
+
+@click.command()
+@click.argument("prompts", nargs=-1)
+@click.option(
+    "--length",
+    "-l",
+    default=30.0,
+    help="Length of the generated audio in seconds",
+    type=float,
+)
+@click.option(
+    "--device",
+    "-d",
+    default="cpu",
+    help="Device to run the model on (e.g., 'cpu', 'cuda')",
+    type=click.Choice(["cpu", "cuda"]),
+)
+@click.option(
+    "--output-folder",
+    "-o",
+    default="output",
+    help="Folder to save the generated audio files",
+    type=click.Path(
+        exists=True,
+        file_okay=False,
+        writable=True,
+        path_type=Path,
+    ),
+)
+def batch(prompts: list[str], length: float, device: str, output_folder: Path) -> None:
+    console = Console()
+
+    with console.status("[bold green]Loading model...[/]", spinner="clock") as status:
+        # Load the model
+        synthesiser = get_model(device=device)
+        status.stop()
+
+    # draw progress bar
+    for prompt in track(prompts, description="Generating music..."):
+        # Generate audio
+        music = make_audio_from_text(synthesiser, prompt, float(length))
+
+        destination = output_folder / f"{uuid.uuid1()}"
+        destination.mkdir(parents=True, exist_ok=True)
+
+        with open(destination / "prompt.txt", "w") as f:
+            f.write(prompt)
+
+        # Save the audio to a file
+        save_audio(music.squeeze(0), f"{str(destination)}/audio", synthesiser)
